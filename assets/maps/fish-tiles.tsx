b23946e67d106b2c65ee3f17ba436f3052921913
leanhhuy0709 <huy.leanh0709@hcmut.edu.vn>
2023-07-04 12:50:02 +0700

    Commit before Big changes

---
+++ assets/maps/fish-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="fish-tiles" tilewidth="128" tileheight="128" tilecount="126" columns="18">
+ <image source="../tiles/fish-tiles.png" width="2304" height="896"/>
+</tileset>
--- assets/maps/kenny/tiles_packed2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="tiles_packed2" tilewidth="8" tileheight="8" tilecount="180" columns="20">
- <image source="../../../../../../../Downloads/Setup Files/tiles_packed.png" width="160" height="72"/>
-</tileset>
